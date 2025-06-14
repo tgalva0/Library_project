f8e6a91bf0df1a1682c354c612fa582a074a5f3d
Thiago Galvao Ferreira <thigalvao2005@gmail.com>
2025-06-14 16:31:25 -0300

    implementação de env

--- src/main/database/MyJDBC.java
+++ src/main/database/MyJDBC.java
@@ -1,13 +1,14 @@
 package main.database;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.*;
 
 public class MyJDBC {
     public static Connection getConnection() {
 
-        String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10783720";
-        String user = "sql10783720";
-        String password = "w7DfdbBBCp";
+        Dotenv dotenv = Dotenv.load();
+        String url = dotenv.get("DB_URL");
+        String user = dotenv.get("DB_USER");
+        String password = dotenv.get("DB_PASSWORD");
 
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
